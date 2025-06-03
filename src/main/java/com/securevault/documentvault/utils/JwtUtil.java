feb928f42673744ee29e88506645f6de1ffa592b
Bhuvesh bansal <96925288+bhuvesh-bansal@users.noreply.github.com>
2025-06-04 00:21:46 +0530

    Fixed errors of libraries

--- src/main/java/com/securevault/documentvault/utils/JwtUtil.java
+++ src/main/java/com/securevault/documentvault/utils/JwtUtil.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.Date;
+import  java.util.Date;
 
 @Component
 public class JwtUtil {
